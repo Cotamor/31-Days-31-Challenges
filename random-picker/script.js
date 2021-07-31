5613cc5f1bdaebf42e101ad3316fbc13ac1654ad
cotamor <cotamor@gmail.com>
2021-08-01 00:16:54 +0900

    Edit layout color, code in js

--- random-picker/script.js
+++ random-picker/script.js
@@ -1,13 +1,45 @@
 const textArea = document.querySelector("textarea");
 const start = document.getElementById("start");
-
 const output = document.getElementById("output");
+let inputElms = [];
+
+textArea.focus();
+
+textArea.addEventListener("keyup", (e) => {
+  createTags(e.target.value);
+});
+
+start.addEventListener("click", () => {
+  console.log(inputElms);
+  getRandomPick(inputElms);
+});
+
+function createTags(elms) {
+  // split text into words, eliminating spaces and commas
+  const elements = elms
+    .split(",")
+    .filter((item) => item.trim() !== "")
+    .map((item) => item.trim());
+  // filter any duplicates from array
+  inputElms = Array.from(new Set(elements));
+  // clear the spans first
+  output.innerHTML = "";
+  inputElms.forEach((item, idx) => {
+    const spanItem = document.createElement("span");
+    spanItem.innerText = item;
+    spanItem.id = idx;
+    output.append(spanItem);
+  });
+}
 
 function getRandomPick(arr) {
   const outputItems = document.querySelectorAll(".output span");
-
+  // clear the previous pick
+  if (outputItems) {
+    outputItems.forEach((item) => (item.classList = ""));
+  }
   // change color of items picked
-  const setIntId = setInterval(() => {
+  const interval = setInterval(() => {
     const ranNum = Math.floor(Math.random() * arr.length);
     // console.log(outputItems[ranNum]);
     outputItems[ranNum].classList.add("picked");
@@ -18,26 +50,10 @@ function getRandomPick(arr) {
 
   // After 5 seconds changing colors end
   setTimeout(() => {
-    const num = Math.floor(Math.random() * arr.length);
-    clearInterval(setIntId);
-    console.log("result", num);
-    outputItems[num].classList.add("picked");
+    clearInterval(interval);
+    setTimeout(() => {
+      const num = Math.floor(Math.random() * arr.length);
+      outputItems[num].classList.add("picked");
+    }, 100);
   }, 5000);
 }
-
-start.addEventListener("click", () => {
-  output.innerHTML = "";
-  // split text into words, eliminating spaces and commas
-  let names = textArea.value.split(",").map((item) => item.trim());
-  // filter duplicates from array
-  names = Array.from(new Set(names));
-  // console.log(names);
-  names.forEach((name, idx) => {
-    const spanItem = document.createElement("span");
-    spanItem.innerText = name;
-    spanItem.id = idx;
-    output.append(spanItem);
-  });
-
-  getRandomPick(names);
-});
