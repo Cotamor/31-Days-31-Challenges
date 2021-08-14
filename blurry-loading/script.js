09e8bd68e430770b004a28a967deaac5cabd903e
cotamor <cotamor@gmail.com>
2021-08-14 23:07:32 +0900

    Fixed bug

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -6,24 +6,26 @@ let opacityNum = 0;
 let blurryNum = 0;
 const interval = setInterval(blurry, 20);
 
-// function blurry() {
-//   count++;
-//   // console.log(count);
-//   text.innerText = `${count}%`;
-//   if (count >= 100) {
-//     clearInterval(interval);
-//   }
+function blurry() {
+  count++;
+  // console.log(count);
+  text.innerText = `${count}%`;
+  if (count >= 100) {
+    clearInterval(interval);
+  }
 
-//   opacityNum = scale(count, 0, 100, 1, 0);
-//   blurryNum = scale(count, 0, 100, 30, 0).toFixed(1);
+  opacityNum = scale(count, 0, 100, 1, 0);
+  blurryNum = scale(count, 0, 100, 30, 0).toFixed(1);
 
-//   console.log(opacityNum, blurryNum);
-//   text.style.opacity = opacityNum;
-//   bg.style.filter = `blur(${blurryNum}px)`;
-// }
+  console.log(opacityNum, blurryNum);
+  text.style.opacity = opacityNum;
+  bg.style.filter = `blur(${blurryNum}px)`;
+}
 
-// function scale(number, inMin, inMax, outMin, outMax) {
-//   return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-// }
+function scale(number, inMin, inMax, outMin, outMax) {
+  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
 // this function is from stack overflow
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 // img
+// https://unsplash.com/@jezael
